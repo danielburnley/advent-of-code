858114c8812853ef6cdecfb8f7bb3488550c97cb
Daniel Jackson <d95jackson@gmail.com>
2016-12-13 20:49:04 +0000

    Day 10 solutionsssss

--- danj/erlang/helpers/helper.hrl
+++ danj/erlang/helpers/helper.hrl
@@ -12,7 +12,22 @@ helper_read_file(Day) ->
     {ok, Data} = file:read_file(FilePath),
     lists:map(fun(W) -> binary_to_list(W) end, binary:split(Data, [<<"\n">>], [global])).
 
-helper_s2i(Str) -> {I, _} = string:to_integer(Str), I.
+%Integer 2 String
+i2s(I) -> integer_to_list(I).
+%String 2 Integer
+s2i(S) -> {I, _} = string:to_integer(S), I.
+
+%String Flatten
+sf(S) -> lists:flatten(S).
+
+%Copare strings as integers "5" > "123" = 5 > 123 = false
+compare_string_integers(S1, S2) -> s2i(sf(S1)) > s2i(sf(S2)).
+
+get_low_high_strings(S1, S2) ->
+	case compare_string_integers(S1, S2) of
+		true -> {sf(S2), sf(S1)};
+		false -> {sf(S1), sf(S2)}
+	end.
 
 helper_multiply_string(Str, 1) -> Str;
 helper_multiply_string(Str, I) -> Str ++ (helper_multiply_string(Str, I - 1)).
